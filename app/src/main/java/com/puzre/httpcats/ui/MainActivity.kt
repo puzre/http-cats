3870fc323fa0352aa521ea8fd0c422ce111e6263
puzre <jose_antoniorivera@outlook.com>
2024-11-16 16:36:04 -0600

    feat: add bottom navigation bar
        - add icons
        - add new fragment called All Cats Fragment
        - add Bottom Navigation View into Activity Main Layout
        - cofigure Navigation Controller in MainActivity
        - create the Bottom Navigation Menu

--- app/src/main/java/com/puzre/httpcats/ui/MainActivity.kt
+++ app/src/main/java/com/puzre/httpcats/ui/MainActivity.kt
@@ -2,11 +2,31 @@ package com.puzre.httpcats.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.puzre.httpcats.R
+import com.puzre.httpcats.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding
+            .navigationBar
+            .setupWithNavController(
+                (supportFragmentManager
+                    .findFragmentById(R.id.fragmentContainerView) as NavHostFragment)
+                    .navController
+            )
+
     }
+
+
 }
